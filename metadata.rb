33cba7a6183f144b99d83ccb8d5784049cd1728b
Daniel Barrell <dbarrell@users.noreply.github.com>
2015-09-11 16:57:45 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures FASTQC'
 long_description 'Installs/Configures FASTQC'
-version '0.1.0'
+version '1.0.1'
 
 depends 'build-essential'
 depends 'magic_shell'
