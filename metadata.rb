066772b95466798bf22e9dab1f560330f9f84a74
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-01-25 14:06:31 +0000

    License, maintainers, description etc

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name 'FastQC'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures FASTQC'
-long_description 'Installs/Configures FASTQC'
+maintainer 'Eagle Genomics'
+maintainer_email 'chef@eaglegenomics.com'
+license 'apache2'
+description 'Installs/Configures FastQC'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.1'
 
 depends 'build-essential'
