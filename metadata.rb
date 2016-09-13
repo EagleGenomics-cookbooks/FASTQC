56565c1b9e37ec3e79eb9995434084b6559211ae
Nick James <nick.james@eaglegenomics.com>
2016-09-13 10:39:32 +0100

    updated to latest FastQC version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@eaglegenomics.com'
 license 'Apache v2.0'
 description 'Installs/Configures FastQC'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.4'
+version '1.0.5'
 
 source_url 'https://github.com/EagleGenomics-cookbooks/FastQC'
 issues_url 'https://github.com/EagleGenomics-cookbooks/FastQC/issues'
